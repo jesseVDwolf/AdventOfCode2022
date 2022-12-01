609945a0dd6692b38c09da5cf94b55d3aab3fe6e
Jesse van der Wolf <jessevanderwolf@gmail.com>
2022-12-01 14:09:26 +0100

    It all looks horrific but I'm feeling a bit sick now.. Just wanted to get it done. Will refactor when I have more energy. (i.e. add poetry, use requests to get the input and rewrite this awefull looking algorithm

---
+++ day1/day1.py
@@ -0,0 +1,24 @@
+from typing import List
+from pathlib import Path
+
+
+file_path = Path(__file__).parent.joinpath('input.txt').resolve()
+with open(file_path, mode='r') as input_file:
+    
+    calorie_list: List[int] = []
+    total_calories_per_elve: List[int] = []
+    for line in input_file:
+        print(line.strip())
+        if line in ['\n', '\r\n']:
+            total_calories_per_elve.append(sum(calorie_list))
+            calorie_list.clear()
+        else:
+            food_item_calorie = int(line.strip())
+            calorie_list.append(food_item_calorie)
+    
+    if len(calorie_list) > 0:
+        total_calories_per_elve.append(sum(calorie_list))
+
+print(max(total_calories_per_elve))
+print(sorted(total_calories_per_elve))
+print(sum(sorted(total_calories_per_elve)[-3:]))
