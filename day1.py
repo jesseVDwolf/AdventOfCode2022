6758aff9ca6982bb189e41e07f4c6fd135626ec7
Jesse van der Wolf <jessevanderwolf@gmail.com>
2022-12-02 15:14:11 +0100

    run pre-commit hooks

--- day1.py
+++ day1.py
@@ -5,14 +5,13 @@ from day import AdventOfCodeDay
 
 
 class AOCDayOne(AdventOfCodeDay):
-
     def __init__(self, session_key: str, day: int, year: int = 2022) -> None:
         super().__init__(session_key, day, year)
 
     def _get_elve_totals(self) -> list[int]:
         if self._stream_input is None:
-            raise ValueError('Stream is not set. Use .set_stream_input() first.')
-        
+            raise ValueError("Stream is not set. Use .set_stream_input() first.")
+
         elve_sum: int = 0
         elve_totals: list[int] = []
         for chunk in self._stream_input.iter_lines():
@@ -21,7 +20,7 @@ class AOCDayOne(AdventOfCodeDay):
                 elve_sum = 0
             else:
                 elve_sum += int(chunk)
-        
+
         return elve_totals
 
     def solve_part_one(self) -> str:
@@ -30,16 +29,17 @@ class AOCDayOne(AdventOfCodeDay):
     def solve_part_two(self) -> str:
         return str(sum(sorted(self._get_elve_totals())[-3:]))
 
+
 def main() -> None:
     if not load_dotenv(override=True):
-        raise ValueError('Could not find .env file')
-    aoc = AOCDayOne(day=1, session_key=os.environ['AOC_SESSION_KEY'])
+        raise ValueError("Could not find .env file")
+    aoc = AOCDayOne(day=1, session_key=os.environ["AOC_SESSION_KEY"])
     aoc.set_input_stream()
-    
+
     print(aoc.solve_part_one())
     aoc.reset_input_stream()
     print(aoc.solve_part_two())
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
